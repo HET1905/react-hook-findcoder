434ddf579339486f452d415c8ae18eed8e66e0e8
Hetal Patel <nphetal1905@gmail.com>
2019-11-07 15:59:42 -0500

    useState to search component

--- src/components/repo/Repoitem.jsx
+++ src/components/repo/Repoitem.jsx
@@ -4,7 +4,7 @@ const Repoitem = ({ repo }) => {
   return (
     // <div className="card" style={{ width: "600px", height: "50px" }}>
     <h2>
-      <a href={repo.url}>{repo.name} </a>
+      <a href={repo.html_url}>{repo.name} </a>
     </h2>
     // </div>
   );
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,66 +1,57 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import PropTypes from "prop-types";
 
 // import Button from "react-bootstrap/Button";
 
-class Search extends Component {
-  state = {
-    text: ""
-  };
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlerts: PropTypes.func.isRequired
-  };
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-  onSubmit = e => {
+const Search = ({ showClear, clearUsers, searchUsers, setAlerts }) => {
+  const [text, setText] = useState("");
+
+  const onChange = e => setText(e.target.value);
+
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlerts("Please enter the words to search..", "light");
+    if (text === "") {
+      setAlerts("Please enter the words to search..", "light");
     } else {
       // passing the props to upper level
-      this.props.searchUsers(this.state.text);
-      this.setState({
-        text: ""
-      });
+      searchUsers(text);
+      setText("");
     }
   };
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <Form onSubmit={this.onSubmit}>
-          <Form.Control
-            type="text"
-            name="text"
-            value={this.state.text}
-            onChange={this.onChange}
-            placeholder="Search coders......."
-          />
-          {/* <Button variant="primary" type="submit">
+
+  return (
+    <div>
+      <Form onSubmit={onSubmit}>
+        <Form.Control
+          type="text"
+          name="text"
+          value={text}
+          onChange={onChange}
+          placeholder="Search coders......."
+        />
+        {/* <Button variant="primary" type="submit">
             Submit
           </Button> */}
 
-          <input type="submit" value="Search" className="btn btn-primary" />
+        <input type="submit" value="Search" className="btn btn-primary" />
 
-          {showClear && (
-            <input
-              type="button"
-              className="btn btn-light"
-              value="Clear"
-              onClick={clearUsers}
-            />
-          )}
-        </Form>
-      </div>
-    );
-  }
-}
-
+        {showClear && (
+          <input
+            type="button"
+            className="btn btn-light"
+            value="Clear"
+            onClick={clearUsers}
+          />
+        )}
+      </Form>
+    </div>
+  );
+};
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlerts: PropTypes.func.isRequired
+};
 export default Search;
